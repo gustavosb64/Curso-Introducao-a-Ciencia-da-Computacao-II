b402e3c5f9aed0ca9858c1e1d9b7808e073ba3cd
Gustavo Barbosa <gustavosb64@gmail.com>
2021-10-11 17:31:19 -0300

    Adicionando Trabalho02: Compressão de Áudio. AINDA NÃO COMEÇADO

---
+++ Trabalho02/main.c
@@ -0,0 +1,16 @@
+/* 
+ * Título: COMPRESSÃO DE ÁUDIO
+ * Nome: Gustavo Siqueira Barbosa
+ * NºUSP: 10728122
+ * Código do curso: SCC0201
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include "./func.c"
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
